b16756be7704f8864a7e4d819c0a2e3c9fe2d9dc
Ashish0014 <138017791+Ashish0014@users.noreply.github.com>
2025-05-30 18:57:03 +0530

    Add src/store/index.ts

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { create } from 'zustand';
+import { User } from '@/types';
+
+interface AppState {
+  user: User | null;
+  isAuthenticated: boolean;
+  setUser: (user: User | null) => void;
+  logout: () => void;
+}
+
+export const useAppStore = create<AppState>((set) => ({
+  user: null,
+  isAuthenticated: false,
+  setUser: (user) => set({ user, isAuthenticated: !!user }),
+  logout: () => set({ user: null, isAuthenticated: false }),
+}));
